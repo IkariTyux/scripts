a236cb270cb7dff8dd7d36a78130b573bdda4c0d
IkariTyux <git@rph.red>
2024-08-04 14:11:41 +0200

    Changed distro detection

--- minecraft.sh
+++ minecraft.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-figlet Minecraft | lolcat
-
 # Define Variables
 Distro=$(cat /etc/os-release | grep -v BUILD_ID | grep ID | sed -s "s/ID=//g")
-LogFile="/home/$USER/.minecraft_install.log"
 TlauncherLocation="/home/$USER/.local/bin"
 TLauncherFile=$(basename *.jar)
-Date=$(date +"%Y-%m-%d - %H:%M:S")
-
-# Add new line to log
-echo "\
-
-$Date\
-
-" >> $LogFile
 
 # Getting the jar file ready
 download_minecraft () {
@@ -26,16 +15,26 @@ rm -f tmp.zip README-EN.txt README-RUS.txt
 download_minecraft 2>> $LogFile
 
 # Installing Java
-if [ "$Distro" = "arch" ]
-  then sudo pacman -Syy --noconfirm jre-openjdk 2>> $LogFile
-elif [ "$Distro" = "debian" ]
-  then sudo apt update && apt install default-jre -y 2>> $LogFile
-elif [ "$Distro" = "fedora" ]
-  then sudo dnf update && dnf install java-11-openjdk.x86_64 -y 2>> $LogFile
-elif [ "$Distro" = "suse" ]
-  then sudo zypperzypper --non-interactive install java-17-openjdk 2>> $LogFile
- else echo "This distro isn't supported"
-fi
+case $Distro in
+  arch|manjaro|endevouros)
+    sudo pacman -Sy
+    sudo pacman -S --noconfirm jre-openjdk
+      ;;
+  debian|ubuntu|linuxmint|pop)
+    sudo apt update 
+    sudo apt install default-jre -y
+      ;;
+  fedora|rhel)
+    sudo dnf update 
+    sudo dnf install java-11-openjdk.x86_64 -y
+      ;;
+  opensuse-leap|opensuse-tumbleweed)
+    sudo zypper --non-interactive install java-17-openjdk
+      ;;
+  *)
+    echo "Your distro isn't supported yet."
+      ;;
+esac
 
 # Copy .desktop file
 curl -s https://raw.githubusercontent.com/IkariTyux/scripts/main/files/minecraft.desktop > /home/$USER/.local/share/applications/minecraft.desktop
@@ -43,5 +42,5 @@ curl -s https://raw.githubusercontent.com/IkariTyux/scripts/main/files/minecraft
 ## Finish
 if [ $? -eq 0 ]
   then echo -e "\033[32m Successfully Installed TLauncher, you may now open it from your app launcher.\033[0m"
-  else echo -e "\033[033m Error in Installation, see logs at $LogFile.\033[0m" && exit
+  else echo -e "\033[033m Error in Installation.\033[0m" && exit
 fi
