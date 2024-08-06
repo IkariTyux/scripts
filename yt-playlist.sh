8de509ad032a949cd1243c0e45fcd83e0875a4dd
IkariTyux <git@rph.red>
2024-08-06 12:29:00 +0200

    Corrected typos related errors

--- yt-playlist.sh
+++ yt-playlist.sh
@@ -3,27 +3,19 @@
 # Variables
 PlaylistUrl=$1
 Format=$2
-PlaylistName=$(curl $PlaylistUrl | awk -F '<meta property="og:title" content="' '{print $2}' | sed 's/\".*//g' | sed '/^[[:space:]]*$/d')
+PlaylistName=$(curl $PlaylistUrl | awk -F '<meta property="og:title" content="' '{print $2}' | sed 's/\".*//g' | sed '/^[[:space:]]*$/d' | sed 's/Album - //g')
 PlalistCover=$(curl $PlaylistUrl)
 Folder="/home/$USER/Videos/$PlaylistName"
-
-# Functions
-## Getting the cover
-getCover () {
-NewUrl=$(echo $PlaylistUrl | sed 's/www/music/g')
-CoverUrl=$(curl $NewUrl | awk -F '<meta property="og:image" content="' '{print$2}' | sed 's/".*//g' | sed '/^[[:space:]]*$/d')
-curl -Lo "$Folder"/cover.jpg $CoverUrl
-}
 
 # Script
 mkdir -p "$Folder"
 
 case $Format in
-  mp4)
+  mp4|mkv|avi)
     yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" --sponsorblock-remove all,-sponsor $PlaylistUrl -P "$Folder"
   ;;
 
-  flac)
+  flac|mp3|ogg)
     yt-dlp -x --audio-format flac $PlaylistUrl -P "$Folder"
   ;;
 
@@ -34,11 +26,17 @@ case $Format in
 esac
 
 ## Rename files
-### Remove YT-DLP's string
+### Remove YT-DLP's strings
 for i in "$Folder"/*; do
   mv -v "$i" "`echo $i | sed 's/\[[^]]*\]//g'`";
 done
-rename " .$Format" ".$Format" "$Folder"/*
+rename " .$Format" ".$Format" "$Folder"/*.$Format
+rename 'Album - ' '' "$Folder"/*
+
+## Getting the cover
+NewUrl=$(echo $PlaylistUrl | sed 's/www/music/g')
+CoverUrl=$(curl $NewUrl | awk -F '<meta property="og:image" content="' '{print$2}' | sed 's/".*//g' | sed '/^[[:space:]]*$/d')
+curl -Lo "$Folder"/cover.jpg $CoverUrl
 
 if [ $? -eq 0 ]
   then echo -e "\033[32m Playlist successfully downloaded \033[0m"
