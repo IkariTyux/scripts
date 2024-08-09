0cb97c041da20323899ee8223d4af542ab79d711
IkariTyux <git@rph.red>
2024-08-09 13:12:18 +0200

    Updated yt-playlists

--- yt-playlist.sh
+++ yt-playlist.sh
@@ -12,11 +12,15 @@ mkdir -p "$Folder"
 
 case $Format in
   mp4|mkv|avi)
-    yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" --sponsorblock-remove all,-sponsor $PlaylistUrl -P "$Folder"
+    yt-dlp -S res,ext:mp4:m4a --recode mp4 --sponsorblock-remove all,-sponsor $PlaylistUrl -P "$Folder"
   ;;
 
   flac|mp3|ogg)
     yt-dlp -x --audio-format flac $PlaylistUrl -P "$Folder"
+    ## Getting the cover
+    NewUrl=$(echo $PlaylistUrl | sed 's/www/music/g')
+    CoverUrl=$(curl $NewUrl | awk -F '<meta property="og:image" content="' '{print$2}' | sed 's/".*//g' | sed '/^[[:space:]]*$/d')
+    curl -Lo "$Folder"/cover.jpg $CoverUrl
   ;;
 
   *)
@@ -31,12 +35,6 @@ for i in "$Folder"/*; do
   mv -v "$i" "`echo $i | sed 's/\[[^]]*\]//g'`";
 done
 rename " .$Format" ".$Format" "$Folder"/*.$Format
-rename 'Album - ' '' "$Folder"/*
-
-## Getting the cover
-NewUrl=$(echo $PlaylistUrl | sed 's/www/music/g')
-CoverUrl=$(curl $NewUrl | awk -F '<meta property="og:image" content="' '{print$2}' | sed 's/".*//g' | sed '/^[[:space:]]*$/d')
-curl -Lo "$Folder"/cover.jpg $CoverUrl
 
 if [ $? -eq 0 ]
   then echo -e "\033[32m Playlist successfully downloaded \033[0m"
