12f479911b76365b43be4bba3b24fbb2415b7433
IkariTyux <git@rph.red>
2024-09-20 16:21:49 +0200

    Separated Videos and Music

--- yt-playlist.sh
+++ yt-playlist.sh
@@ -5,18 +5,19 @@ PlaylistUrl=$1
 Format=$2
 PlaylistName=$(curl $PlaylistUrl | awk -F '<meta property="og:title" content="' '{print $2}' | sed 's/\".*//g' | sed '/^[[:space:]]*$/d' | sed 's/Album - //g')
 PlalistCover=$(curl $PlaylistUrl)
-Folder="/home/$USER/Videos/$PlaylistName"
+FolderMusic="/home/$USER/Music/$PlaylistName"
+FolderVideos="/home/$USER/Videos/$PlaylistName"
 
 # Script
 mkdir -p "$Folder"
 
 case $Format in
   mp4|mkv|avi)
-    yt-dlp -S res,ext:mp4:m4a --recode mp4 --sponsorblock-remove all,-sponsor $PlaylistUrl -P "$Folder"
+    yt-dlp -S res,ext:mp4:m4a --recode mp4 --sponsorblock-remove all,-sponsor $PlaylistUrl -P "$FolderVideos"
   ;;
 
   flac|mp3|ogg)
-    yt-dlp -x --audio-format flac $PlaylistUrl -P "$Folder"
+    yt-dlp -x --audio-format flac $PlaylistUrl -P "$FolderMusic"
     ## Getting the cover
     NewUrl=$(echo $PlaylistUrl | sed 's/www/music/g')
     CoverUrl=$(curl $NewUrl | awk -F '<meta property="og:image" content="' '{print$2}' | sed 's/".*//g' | sed '/^[[:space:]]*$/d')
