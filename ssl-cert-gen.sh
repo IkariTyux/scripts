2251ec2c9151b45fdb1f23314acad7b0d9f8e072
shinji <git@rph.red>
2024-12-16 10:58:45 +0100

    Updated script, and added path specification

--- ssl-cert-gen.sh
+++ ssl-cert-gen.sh
@@ -4,6 +4,7 @@
 ## App name and domain
 App=$1 # name of the app (lower case, no spaces, only "-" or "_").
 Domain=$2 # Fully qualified Domain name
+AppPath=$3 # Path for the Cert
 
 ## Organisation
 # Can be changed, or left as it.
@@ -13,33 +14,33 @@ City="Tokyo III" # The city
 Organisation="Nerv" # The name of the organisation. Can be anything
 OrgUnit="IT Department" # Whatever you want it to be
 
-## Paths
-SSLPath="/home/$USER/.SSL"
-AppPath="$SSLPath/$App"
-
 ### Creation of the folder that stores the cert.
-mkdir -p $AppPath
-echo "Generating a certificate for $App with $Domain"
+## Checks if a Path as been defined. If it's the case, creates it.
+## If not, uses current direcory.
+if [ ! -z "${AppPath}" ]
+then
+  mkdir -p $AppPath
+else
+  AppPath="."
+fi
+
+echo "Generating a new certificate"
 echo ""
 
-### Function that requests the certificate using openssl
-# Source: https://linuxize.com/post/creating-a-self-signed-ssl-certificate
-# The -subj parameters uses the Organisation variables
-# It stores the certificate and key in the folder named after the $App variable
-
 ## Requesting certificate
-openssl req -newkey rsa:4096 \
-            -x509 \
-            -sha256 \
-            -days 3650 \
-            -nodes \
-            -out $AppPath/cert.pem \ 
-            -keyout $AppPath/key.pem \
-            -subj "/C=$Country/ST=$State/L=$City/O=$Organisation/OU=$OrgUnit/CN=$Domain"
+openssl req -new \
+	-newkey rsa:4096 \
+	-x509 \
+        -sha256 \
+        -days 3650 \
+        -nodes \
+        -out $AppPath/cert.pem \
+	-keyout $AppPath/key.pem \
+        -subj "/C=$Country/ST=$State/L=$City/O=$Organisation/OU=$OrgUnit/CN=$Domain"
 
 ### End of the script
-# checks if cert_gen ran successfully or not an outputs the result
+# checks if the command ran successfully or not an outputs the result
 if [ $? -eq 0 ]
-  then echo -e "\033[32m The certificate is now available at $AppPath \033[0m"
+  then echo -e "\033[32m The new certificate is now available at $AppPath \033[0m"
   else echo -e "\033[033m Error while generating the certificate \033[0m" && exit
 fi
