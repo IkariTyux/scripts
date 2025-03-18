da073f3557432a670251c22d51885bf5d7892d53
IkariTyux <git@rph.red>
2025-03-18 16:40:37 +0100

    Restored to normal working state and added multiples formats

--- yt-playlist.sh
+++ yt-playlist.sh
@@ -8,19 +8,32 @@ FolderMusic="/home/$USER/Music/$PlaylistName"
 FolderVideos="/home/$USER/Videos/$PlaylistName"
 
 # Script
-mkdir -p "$Folder"
 
 case $Format in
   mp4|mkv|avi)
-    yt-dlp -S res,ext:mp4:m4a --recode mp4 --sponsorblock-remove all,-sponsor $PlaylistUrl -P "$FolderVideos"
+    yt-dlp -S res,ext:$Format:m4a --recode mp4 --sponsorblock-remove all,-sponsor $PlaylistUrl -P "$FolderVideos"
+    
+    ## Rename files
+    for i in "$FolderVideos"/*; do
+      mv -v "$i" "`echo $i | sed 's/\[[^]]*\]//g'`";
+    done
+    rename " .$Format" ".$Format" "$FolderVideos"/*.$Format
   ;;
 
   flac|mp3|ogg)
-    yt-dlp -x --audio-format flac $PlaylistUrl -P "$FolderMusic"
+    yt-dlp -x --audio-format $Format $PlaylistUrl -P "$FolderMusic"
+    
     ## Getting the cover
     NewUrl=$(echo $PlaylistUrl | sed 's/www/music/g')
     CoverUrl=$(curl $NewUrl | awk -F '<meta property="og:image" content="' '{print$2}' | sed 's/".*//g' | sed '/^[[:space:]]*$/d')
-    curl -Lo "$Folder"/cover.jpg $CoverUrl
+    curl -Lo "$FolderMusic"/cover.jpg $CoverUrl
+    
+    ## Rename files
+    ### Remove YT-DLP's strings
+    for i in "$FolderMusic"/*; do
+      mv -v "$i" "`echo $i | sed 's/\[[^]]*\]//g'`";
+    done
+    rename " .$Format" ".$Format" "$FolderMusic"/*.$Format
   ;;
 
   *)
@@ -29,15 +42,11 @@ case $Format in
   ;;
 esac
 
-## Rename files
-### Remove YT-DLP's strings
-for i in "$Folder"/*; do
-  mv -v "$i" "`echo $i | sed 's/\[[^]]*\]//g'`";
-done
-rename " .$Format" ".$Format" "$Folder"/*.$Format
-
 if [ $? -eq 0 ]
-  then echo -e "\033[32m Playlist successfully downloaded \033[0m"
-    notify-send --icon=terminal --app-name=Bash 'Playlist Downloaded' "Finished downloading $PlaylistName!"
-  else echo -e "\033[033m Error while downloading the playlist \033[0m" && exit
+then
+  echo -e "\033[32m Playlist successfully downloaded \033[0m"
+  notify-send --icon=terminal --app-name=Bash 'Playlist Downloaded' "Finished downloading $PlaylistName!"
+else
+  echo -e "\033[033m Error while downloading the playlist \033[0m"
+  exit
 fi
